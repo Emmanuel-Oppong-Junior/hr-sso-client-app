542917d4cee075109a1c1e3c04c0534289b50f9a
Emmanuel Oppong Junior <117347194+Emmanuel-Oppong-Junior@users.noreply.github.com>
2024-01-26 15:51:09 +0000

    add changes

--- src/components/protected-route/ProtectedRoute.tsx
+++ src/components/protected-route/ProtectedRoute.tsx
@@ -2,7 +2,7 @@ import { Outlet, Navigate } from "react-router-dom";
 import Cookies from "js-cookie";
 
 const ProtectedRoutes: React.FC = () => {
-  return Cookies.get("ms-token") ? <Outlet /> : <Navigate to="/login" />;
+  return Cookies.get("app-token") ? <Outlet /> : <Navigate to="/login" />;
 };
 
 export default ProtectedRoutes;
